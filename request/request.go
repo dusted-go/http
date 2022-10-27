34ec41845e09b83860e1ce11f42666851fb1a657
Dustin Moris Gorski <dustinmoris@gmail.com>
2022-10-27 22:39:33 +0100

    Removed ShiftPath from request

--- request/request.go
+++ request/request.go
@@ -48,13 +48,3 @@ func FullURL(r *http.Request) string {
 func HTTPSURL(r *http.Request) string {
 	return fullURL(r, "https")
 }
-
-// ShiftPath splits off the first component of path.
-// Head will never contain a slash and tail will always be a rooted path without trailing slash.
-func ShiftPath(path string) (head, tail string) {
-	i := strings.Index(path[1:], "/") + 1
-	if i <= 0 {
-		return path[1:], "/"
-	}
-	return path[1:i], path[i:]
-}
